288707a228da211d84c20ed5d6c34c525f1f22d2
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2023-01-19 13:51:55 -0800

    profile scren

--- frontend/src/localStorage.js
+++ frontend/src/localStorage.js
@@ -27,6 +27,9 @@ export const getCartItems = () => {
     })
   );
 };
+export const clearUser = () => {
+  localStorage.removeItem('userInfo');
+};
 export const getUserInfo = () => {
   return localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
